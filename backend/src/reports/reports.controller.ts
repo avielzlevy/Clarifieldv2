956fc6ed82856f73f4c49aabcf46c3d87ef17ed3
MeToRaFRev <MeToRaFRev@gmail.com>
2025-04-16 18:26:03 +0300

    asd

--- backend/src/reports/reports.controller.ts
+++ backend/src/reports/reports.controller.ts
@@ -3,7 +3,6 @@ import { Controller, Get, Post, Put, Param, Body } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { AddReportDto, UpdateReportDto } from './reports.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
-
 @ApiTags('reports')
 @Controller('reports')
 export class ReportsController {
